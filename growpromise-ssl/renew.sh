d1178620fc4f5c5417842510fe5b450bae996052
ho8ae <xogh2242@gmail.com>
2025-06-03 11:20:12 +0000

    refactor: 필요없는 파일 삭제

--- growpromise-ssl/renew.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "🔄 Renewing SSL certificates..."
-cd ~/growpromise-ssl
-docker-compose -f docker-compose-ssl.yml run --rm certbot renew
-docker-compose -f docker-compose-ssl.yml restart nginx-ssl
-echo "✅ Certificate renewal completed!"
